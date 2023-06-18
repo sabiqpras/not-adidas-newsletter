b7f7df5d5f766ab4527c2b9a20b567c2aae1203d
Sabiq Prasetyo <106587308+sabiqpras@users.noreply.github.com>
2023-06-18 23:57:07 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function (req, res) {
+    // Raw Data
+    const userFirstName = req.body.firstName;
+    const userLastName = req.body.lastName;
+    const userEmail = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: userEmail,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: userFirstName,
+                    LNAME: userLastName,
+                },
+            },
+        ],
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    // Mailchimp API
+    const urlMailchimp = "https://us21.api.mailchimp.com/3.0/lists/a7506373a9";
+    const options = {
+        method: "POST",
+        auth: "sabiq:00962bc70a8947c8d8e1737bd432bdab",
+    };
+
+    const request = https.request(urlMailchimp, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        } else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/success", function (req, res) {
+    res.redirect("/");
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server is running on port 3000");
+});
+
+// apikey
+// 00962bc70a8947c8d8e1737bd432bdab-us21
+
+// list Id
+// a7506373a9
